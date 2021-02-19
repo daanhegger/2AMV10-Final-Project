3c17817abdfe9088bd55aa234591972f997e2c39
Daan Hegger <daanhegger@gmail.com>
2021-02-19 15:06:16 +0100

    backend data import

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,21 @@
 from flask import Flask
-app = Flask(__name__)
 from flask_cors import CORS
+import os
+import pandas as pd
+from os import path
 
+# Flask
+app = Flask(__name__)
 # Allow cross origin requests
 CORS(app)
 
+dataset_path = os.environ.get('DATASET_PATH')
+
+if not dataset_path or not path.exists(dataset_path):
+    raise ValueError("You need to set DATASET_PATH")
+
+df = pd.read_csv(dataset_path)
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return f"Imported dataset from {dataset_path}"
