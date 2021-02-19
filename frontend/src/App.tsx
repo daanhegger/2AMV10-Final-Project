127ce64026c565f59bd8456b84c36be8a6d15b54
Daan Hegger <daanhegger@gmail.com>
2021-02-19 14:08:49 +0100

    initialize react app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import useAxios from "axios-hooks";
+
+function App() {
+  const [{ data, loading, error }] = useAxios(
+      'http://localhost:5000'
+  )
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error!</p>
+
+  return (
+    <div className="App">
+      {data}
+    </div>
+  );
+}
+
+export default App;
