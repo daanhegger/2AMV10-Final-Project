dc6fdbe9566be393e20002160a64e5d4cefc0e87
Daan Hegger <daanhegger@gmail.com>
2021-02-19 15:06:39 +0100

    add prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
+import React from "react";
 import useAxios from "axios-hooks";
 
 function App() {
-  const [{ data, loading, error }] = useAxios(
-      'http://localhost:5000'
-  )
+  const [{ data, loading, error }] = useAxios("http://localhost:5000");
 
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error!</p>
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
 
-  return (
-    <div className="App">
-      {data}
-    </div>
-  );
+  return <div className="App">{data}</div>;
 }
 
 export default App;
