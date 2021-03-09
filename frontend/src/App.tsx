8f15a2ecf6dbb5c17439424bc3c2c20caf8601bf
Daan Hegger <daanhegger@gmail.com>
2021-03-09 14:31:12 +0100

    prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,38 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import {Card, Typography} from "@material-ui/core";
-import useAxios from "axios-hooks"
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { Card, Typography } from "@material-ui/core";
+import useAxios from "axios-hooks";
 import Chart from "./Components/makeChart";
-import Grid from '@material-ui/core/Grid';
-import Slider from '@material-ui/core/Slider';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox, { CheckboxProps } from '@material-ui/core/Checkbox';
+import Grid from "@material-ui/core/Grid";
+import Slider from "@material-ui/core/Slider";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox, { CheckboxProps } from "@material-ui/core/Checkbox";
+
+//Dummie data for the line/scatter chart
+const labels = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+const dataDummies = [5, 9, 3, 5, 2, 3, 1];
+
+let dataDummies2: any[] = [];
+for (let i = 0; i < 1000; i++) {
+  dataDummies2.push({
+    x: Math.random() * Math.floor(10),
+    y: Math.random() * Math.floor(10),
+  });
+}
 
 function App() {
-  const [{ data, loading, error }] = useAxios(
-      'http://localhost:5000'
-  )
+  const [{ data, loading, error }] = useAxios("http://localhost:5000");
 
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error!</p>
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
 
   // const paramsTitle = (
   //     <Typography variant="h6" component="h1">
@@ -22,11 +40,10 @@ function App() {
   //     </Typography>
   // );
 
-
   const GreenCheckbox = withStyles({
     root: {
       color: "#ff9191",
-      '&$checked': {
+      "&$checked": {
         color: "#FF9191",
       },
     },
@@ -34,77 +51,76 @@ function App() {
   })((props: CheckboxProps) => <Checkbox color="default" {...props} />);
 
   // Dummie words. Need to be changed with the most frequent names/topics from the actual data.
-  const words = ['word 1', 'word 2', 'word 3', 'word 4', 'word 5', 'word 6', 'word 7', 'word 8', 'word 9', 'word 10'];
+  const words = [
+    "word 1",
+    "word 2",
+    "word 3",
+    "word 4",
+    "word 5",
+    "word 6",
+    "word 7",
+    "word 8",
+    "word 9",
+    "word 10",
+  ];
 
   const renderCheckboxesCard = () => {
-      return (
-          <div>
-            {words.map(word => (
-              <FormControlLabel
-                  control={<GreenCheckbox checked={false} name="checkedG"/>}
-                  label={word}
-              />
-              ))}
-          </div>
-      )
+    return (
+      <div>
+        {words.map((word) => (
+          <FormControlLabel
+            control={<GreenCheckbox checked={false} name="checkedG" />}
+            label={word}
+          />
+        ))}
+      </div>
+    );
   };
 
-  //Dummie data for the line/scatter chart
-  const labels = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-  const dataDummies = [5, 9, 3, 5, 2, 3, 1];
-
-  let dataDummies2 = []
-  for(let i = 0; i<1000; i++) {
-    dataDummies2.push({x:Math.random()*Math.floor(10), y:Math.random()*Math.floor(10)})
-  }
-
   return (
-      <div className="App" >
-        <Grid container spacing={2}>
-          <Grid item xs={9}>
-            <div>
-              <Card className="vectorized-words-charts">
-                <Grid container spacing={3}>
-                  <Grid item xs={6}>
-
-                    <Chart
-                        type={'line'}
-                        ctx={'lineChart'}
-                        labels={labels}
-                        chartData={dataDummies}
-                        backgroundColor={"#FF9191"}
-                        chartLabel={"occurance"}
-                    />
-
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Chart
-                        type={'scatter'}
-                        ctx={'scatterChart'}
-                        chartData={dataDummies2}
-                        backgroundColor={"#AEBBFF"}
-                        chartLabel={"occurance"}
-                    />
-
-                  </Grid>
+    <div className="App">
+      <Grid container spacing={2}>
+        <Grid item xs={9}>
+          <div>
+            <Card className="vectorized-words-charts">
+              <Grid container spacing={3}>
+                <Grid item xs={6}>
+                  <Chart
+                    type={"line"}
+                    ctx={"lineChart"}
+                    labels={labels}
+                    chartData={dataDummies}
+                    backgroundColor={"#FF9191"}
+                    chartLabel={"occurance"}
+                  />
                 </Grid>
-              </Card>
-            </div>
-          </Grid>
-          <Grid item xs={3}>
-            <div>
-              <Card className="set-vectorized-parameters">
-                {/*set parameters for vectorization*/}
-                {renderCheckboxesCard()}
-              </Card>
-              <Card className="set-words-topics" style={{marginTop: "3em"}}>
-                {renderCheckboxesCard()}
-              </Card>
-            </div>
-          </Grid>
+                <Grid item xs={6}>
+                  <Chart
+                    type={"scatter"}
+                    ctx={"scatterChart"}
+                    chartData={dataDummies2}
+                    backgroundColor={"#AEBBFF"}
+                    chartLabel={"occurance"}
+                  />
+                </Grid>
+              </Grid>
+            </Card>
+          </div>
         </Grid>
-      </div>
+        <Grid item xs={3}>
+          <div>
+            <Card className="set-vectorized-parameters">
+              {/*set parameters for vectorization*/}
+              {renderCheckboxesCard()}
+            </Card>
+            <Card className="set-words-topics" style={{ marginTop: "3em" }}>
+              {renderCheckboxesCard()}
+            </Card>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
   );
-};
+}
 
 export default App;
--- frontend/src/Components/makeChart.tsx
+++ frontend/src/Components/makeChart.tsx
@@ -1,46 +1,47 @@
-import React, { useEffect } from 'react';
-import Chartjs from 'chart.js';
+import React, { useEffect } from "react";
+import Chartjs from "chart.js";
 
 /* LineChart component. It expects the following imput:
-*  - type: the type of chart to make (e.g. 'line', 'scatter')
-*  - labels: x-axis names.
-*  - chartData: the data to plot.
-*  - backgroundColor: the color of the line or the data points
-*  - chartLabel: name of the data for in the legend.
-*  */
+ *  - type: the type of chart to make (e.g. 'line', 'scatter')
+ *  - labels: x-axis names.
+ *  - chartData: the data to plot.
+ *  - backgroundColor: the color of the line or the data points
+ *  - chartLabel: name of the data for in the legend.
+ *  */
 
 const Chart = ({
-                   type,
-                   ctx,
-                   labels,
-                   chartData = [],
-                   backgroundColor,
-                   chartLabel = null,
-               }:any) => {
+  type,
+  ctx,
+  labels,
+  chartData = [],
+  backgroundColor,
+  chartLabel = null,
+}: any) => {
+  const data: any = {
+    labels,
+    datasets: [
+      {
+        label: chartLabel,
+        data: chartData,
+        backgroundColor: backgroundColor,
+      },
+    ],
+  };
 
-    const data:any = {
-        labels,
-        datasets: [{
-            label: chartLabel,
-            data: chartData,
-            backgroundColor: backgroundColor,
-        }]
-    };
+  const chartConfig = {
+    type: type,
+    data: data,
+  };
 
-    const chartConfig = {
-        type: type,
-        data: data,
-    };
+  useEffect(() => {
+    new Chartjs(ctx, chartConfig);
+  });
 
-    useEffect(() => {
-        new Chartjs(ctx, chartConfig);
-    });
-
-    return (
-        <div>
-            <canvas id={ctx} width="200" height="200"/>
-        </div>
-    );
+  return (
+    <div>
+      <canvas id={ctx} width="200" height="200" />
+    </div>
+  );
 };
 
 export default Chart;
