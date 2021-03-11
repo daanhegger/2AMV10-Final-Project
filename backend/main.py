ecd689b05477b1a63b8097d22b209e2bd663512c
Daan Hegger <daanhegger@gmail.com>
2021-03-11 20:55:43 +0100

    create route for filtered volume histogram

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-import json
 from flask import Flask
 from flask.cli import load_dotenv
 from flask_cors import CORS
@@ -7,12 +6,9 @@ import pandas as pd
 from os import path
 from models.vectorize_words import init_word2vec, get_word2vec_vectors
 from flask import jsonify
+from preprocess import init_df, df
 
-# Flask
-app = Flask(__name__)
-# Allow cross origin requests
-CORS(app)
-
+# Load .env variables
 load_dotenv()
 
 dataset_path = os.environ.get('DATASET_PATH')
@@ -20,12 +16,24 @@ dataset_path = os.environ.get('DATASET_PATH')
 if not dataset_path or not path.exists(dataset_path):
     raise ValueError("You need to set DATASET_PATH")
 
-# Import dataframe only once
-df = pd.read_csv(dataset_path)
+init_df(dataset_path)
+print("Loaded dataset into memory")
 
 # Initialize and save a word2vec model only once
 if not path.exists("./storage/word2vec.model"):
     init_word2vec(df)
+    print("Initialized word2vec model")
+else:
+    print("word2vec model already exists")
+
+# Flask
+app = Flask(__name__)
+# Allow cross origin requests
+CORS(app)
+
+# Add routes from other files
+from routes.volume import volume_routes
+app.register_blueprint(volume_routes)
 
 
 @app.route('/')
@@ -38,3 +46,4 @@ def word2vec():
     return jsonify(
         [(str(x), str(y), w) for x, y, w in get_word2vec_vectors()]
     )
+
--- backend/preprocess.py
+++ backend/preprocess.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+df = None
+
 
 def preprocess(df: pd.DataFrame):
     """Preprocess the dataset in-place"""
@@ -9,3 +11,12 @@ def preprocess(df: pd.DataFrame):
 
     # Replace nan's (float) to empty strings s.t. text processing works for every row
     df.message = df.message.fillna('')
+
+
+def init_df(dataset_path):
+    """Load data into memory, usable by rest of the app"""
+
+    # Import dataframe only once
+    global df
+    df = pd.read_csv(dataset_path)
+    preprocess(df)
