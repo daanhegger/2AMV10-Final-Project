12e292d0a17e4b8e0042d7c8ec5e8ea08dbfbb8b
Daan Hegger <daanhegger@gmail.com>
2021-03-12 15:32:23 +0100

    collapseble sidebar improvement

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,114 @@
-import { AppBar, Container } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Container,
+  createStyles,
+  Drawer,
+  makeStyles,
+  Theme,
+} from "@material-ui/core";
 import MainTool from "./Components/MainTool";
-
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import clsx from "clsx";
+
+const drawerWidth = 400;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    content: {
+      flexGrow: 1,
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginRight: -drawerWidth,
+    },
+    contentShift: {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginRight: 0,
+    },
+  })
+);
 
 function App() {
+  const classes = useStyles();
+  const [sidebar, setSidebar] = useState<boolean>(true);
+
   return (
-    <div>
-      <AppBar position="static">
+    <div className={classes.root}>
+      {/* Top bar with title of tool and button to expand/collapse sidebar */}
+      <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
           <Typography style={{ flexGrow: 1 }} variant="h6">
             Tweet-Analyzer
           </Typography>
-          <IconButton edge="end" color="inherit" aria-label="menu">
+          <IconButton
+            edge="end"
+            color="inherit"
+            onClick={() => setSidebar(!sidebar)}
+          >
             <MenuIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
-      <Container>
-        <div style={{ padding: "20px 0" }}>
-          <MainTool />
-        </div>
-      </Container>
+
+      {/* Center part of the tool */}
+      <main
+        // Conditional classes to adapt to "opened" state of the sidebar
+        className={clsx(classes.content, {
+          [classes.contentShift]: sidebar,
+        })}
+      >
+        <Container>
+          {/* Empty toolbar to compensate the height of the appbar */}
+          <Toolbar />
+
+          <div style={{ padding: "20px 0" }}>
+            {/* Main content of the page: the visualizations */}
+            <MainTool />
+          </div>
+        </Container>
+      </main>
+
+      {/* Collapsable sidebar */}
+      <Drawer
+        open={sidebar}
+        variant="persistent"
+        anchor="right"
+        className={classes.drawer}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        {/* Empty toolbar to compensate the height of the appbar */}
+        <Toolbar />
+
+        {/* Content of the sidebar */}
+        <ul>
+          {new Array(100).fill(undefined).map((_, i) => (
+            <li key={i}>Topic item {i}</li>
+          ))}
+        </ul>
+      </Drawer>
     </div>
   );
 }
