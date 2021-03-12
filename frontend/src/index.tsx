40b37b201f8c614297d3c54cea51a32a53503220
Daan Hegger <daanhegger@gmail.com>
2021-03-12 14:56:07 +0100

    redo layout and styling

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+// Import material font
+import "fontsource-roboto";
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* CssBaseline: Change default css properties to make page more material-like */}
+    <CssBaseline />
     <App />
   </React.StrictMode>,
 
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
