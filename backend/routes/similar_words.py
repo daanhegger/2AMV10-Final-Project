dc9b09ff2ee2d18081a30e7476ede464e50f2721
Daan Hegger <daanhegger@gmail.com>
2021-03-14 19:40:35 +0100

    document and deduplicate

--- backend/routes/similar_words.py
+++ backend/routes/similar_words.py
@@ -15,34 +15,48 @@ CORS(similar_words_routes)
 
 @similar_words_routes.route("/similar-words")
 def volume():
+    # User request similar words for a given "seed" word
     query = request.args.get('query')
 
+    # Reject if no seed is given
     if query is None:
         abort(400, "No query specified")
 
+    # Generate word vector for seed
     word_doc = nlp(query)
+    # Amount of similar words to compute
     topn = 100
 
+    # Find all similar word vectors based no cosine similarity
     keys, best_rows, scores = nlp.vocab.vectors.most_similar(
         word_doc.vector.reshape(1, word_doc.vector.shape[0]),
         n=topn
     )
 
+    # Find the actual words for vectors
     words = [nlp.vocab.strings[w] for w in keys[0]]
+    # Extract the 300 dimension vectors
     vectors = np.array([nlp.vocab.vectors[w] for w in keys[0]])
 
+    # Dimensionality reduction
     pca = PCA(n_components=2)
-
     vectors_2d = pca.fit_transform(vectors)
 
+    # Convert data to easy format
     data = [
         {
-            "word": w,
+            "word": w.lower(),
             "vector": [str(c) for c in v],
             "score": str(s)
         }
         for w, v, s in zip(words, vectors_2d, scores[0])
     ]
 
-    return jsonify(data)
+    # De duplicate tokens, keep the highest one
+    de_duplicated = []
+    for token in data:
+        if token["word"] not in [t["word"] for t in de_duplicated]:
+            de_duplicated.append(token)
+
+    return jsonify(de_duplicated)
 
