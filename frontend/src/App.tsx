8685489b1484d51ac169b6b3851a9e87cbd84453
Alina Vorobiova <alinavorobyova8@gmail.com>
2021-03-22 10:45:42 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -43,26 +43,6 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-function TabPanel(props: any) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
 function a11yProps(index: any) {
   return {
     id: `simple-tab-${index}`,
@@ -93,8 +73,6 @@ function App() {
           </IconButton>
         </Toolbar>
       </AppBar>
-      <TabPanel value={value} index={0}/>
-      <TabPanel value={value} index={1}/>
 
       {/* Center part of the tool */}
       <main
